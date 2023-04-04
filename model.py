21aa5e8a80a0e5cd5728ab56b66fff9298c92c00
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-04 23:31:32 +0530

    efficientnet b7

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.models import Model
+from efficientnet.tfkeras import EfficientNetB7
 
 # Define the training and test data directories
 train_dir = 'Dataset/train'
@@ -7,9 +10,14 @@ test_dir = 'Dataset/test'
 
 # Define the data generators for training and validation
 train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=20,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
                                    shear_range=0.2,
                                    zoom_range=0.2,
-                                   horizontal_flip=True)
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(train_dir,
@@ -21,27 +29,20 @@ test_generator = test_datagen.flow_from_directory(test_dir,
                                                   batch_size=32,
                                                   class_mode='categorical')
 
-# Define the CNN architecture
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(5, activation='softmax')
-])
+# Define the EfficientNetB7 model as the base model
+base_model = EfficientNetB7(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
+
+# Add new layers on top of the base model for classification
+x = GlobalAveragePooling2D()(base_model.output)
+x = Dense(128, activation='relu')(x)
+predictions = Dense(5, activation='softmax')(x)
+
+# Combine the base model with the new layers
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Freeze the base model layers
+for layer in base_model.layers:
+    layer.trainable = False
 
 # Compile the model
 model.compile(optimizer='adam',
@@ -50,7 +51,7 @@ model.compile(optimizer='adam',
 
 # Train the model
 history = model.fit(train_generator,
-                    epochs=20,
+                    epochs=13,
                     validation_data=test_generator)
 
 # Evaluate the model on the test set
@@ -58,4 +59,4 @@ test_loss, test_acc = model.evaluate(test_generator)
 print('Test accuracy:', test_acc)
 
 # Saving the model
-model.save('face_mask_classification.h5')
+model.save('face_mask_classification_efficientnetb7.h5')
